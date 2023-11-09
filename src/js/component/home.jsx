ea23da02458ba98123416d9ece756283997adbf9
JoseR4Geeks <143768985+JoseR4Geeks@users.noreply.github.com>
2023-11-09 01:53:19 +0000

    code done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  return (
+    <div className="container">
+      <h1>My Todos</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            placeholder="What do you need to do?"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                setTodos(todos.concat(inputValue));
+                setInputValue("");
+              }
+            }}
+          />
+        </li>
+        {todos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <FontAwesomeIcon
+              icon={faX}
+              onClick={() =>
+                setTodos(
+                  todos.filter((t, currentIndex) => index !== currentIndex)
+                )
+              }
+              style={{ color: "#ff0000" }}
+			  className="faIcon"
+            />
+          </li>
+        ))}
+      </ul>
+      <div>{todos.length} tasks</div>
+    </div>
+  );
 };
 
 export default Home;
